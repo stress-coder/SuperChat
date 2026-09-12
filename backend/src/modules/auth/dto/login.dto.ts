30426b03fdc4902a357c34cf52616fc705e6c562
MD Shahidul Bhuiyan ( Shawon ) <mdshahidul3244@gmail.com>
2026-09-12 02:18:52 +0200

    typescript version update

--- backend/src/modules/auth/dto/login.dto.ts
+++ backend/src/modules/auth/dto/login.dto.ts
@@ -3,9 +3,9 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class LoginDto {
   @IsEmail({}, { message: 'Email must be a valid email address.' })
   @IsNotEmpty({ message: 'Email is required.' })
-  email: string;
+  email!: string;
 
   @IsString({ message: 'Password must be a string.' })
   @IsNotEmpty({ message: 'Password is required.' })
-  password: string;
+  password!: string;
 }
